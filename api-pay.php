ad5913bca3e7a151c6e61d8c59ef8499649937f4
KodyxView-MZ <mozdrop.moz@gmail.com>
2026-05-01 00:21:36 +0200

    Add files via upload

---
+++ api-pay.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+$token = "1897|gr4QbJSY8cMV4qsrTvRdqpAq0BNDshHFQhfh0ukt4ce3237f";
+$apiUrl = "https://api.paysuite.co.mz/v1/payments";
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$ch = curl_init($apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer ' . $token,
+    'Content-Type: application/json'
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($input));
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpCode);
+echo $response;
+?>
